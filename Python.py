0657ae92469e4ca55e8d6f701458a6e6bd294e61
Shreyaharish <168474572+Shreyaharish@users.noreply.github.com>
2024-06-18 09:55:53 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,13 +1,5 @@
-x = 24
-   
-if x % 24 == 0:
-    print(x,"Is Even Number")
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
 else:
-    print(x, "Is Odd Number")
-     
-y = 19
-   
-if y % 19 == 0:
-    print(y,"Is Even Number")
-else:
-    print(y, "Is Odd Number")
+   print("{0} is Odd".format(num))
